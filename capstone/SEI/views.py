48eee691b1417048de0caf8cfd01cba3e2f8c5a3
eccco_yao <eccco_yao@eccco-yaodeMBP.wv.cc.cmu.edu>
2016-11-12 20:14:29 -0500

    add project_overview and budget_view method

--- capstone/SEI/views.py
+++ capstone/SEI/views.py
@@ -12,13 +12,17 @@ from django.contrib.auth import login, authenticate
 
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
+import json
+import datetime
 
 from SEI.models import *
 from SEI.forms import *
 
+
 @login_required
 def home(request):
-    return render(request,'SEI/home.html', {})
+    return render(request, 'SEI/home.html', {})
+
 
 @login_required
 def projectview(request, PWP_num):
@@ -26,6 +30,7 @@ def projectview(request, PWP_num):
     project_item = get_object_or_404(Project, PWP_num=PWP_num)
     context['project'] = project_item
     return render(request, 'SEI/projectview.html', context)
+
 
 @login_required
 @transaction.atomic
@@ -43,13 +48,14 @@ def add_project(request):
         return render(request, 'SEI/project.html', context)
 
     new_project = Project(PWP_num=form.cleaned_data['PWP_num'], \
-                            project_description=form.cleaned_data['project_description'],\
-                            project_budget=form.cleaned_data['project_budget'], \
-                            is_internal=form.cleaned_data['is_internal'], \
-                            start_date=form.cleaned_data['start_date'], \
-                            end_date=form.cleaned_data['end_date'])
+                          project_description=form.cleaned_data['project_description'], \
+                          project_budget=form.cleaned_data['project_budget'], \
+                          is_internal=form.cleaned_data['is_internal'], \
+                          start_date=form.cleaned_data['start_date'], \
+                          end_date=form.cleaned_data['end_date'])
     new_project.save()
     return redirect('projectview', PWP_num=form.cleaned_data['PWP_num'])
+
 
 ##not working
 @login_required
@@ -92,6 +98,7 @@ def profile(request, user_name):
 
     return render(request, 'SEI/employee.html', context)
 
+
 ##still need to check whether normal user can edit his/her own file, should have one more parameter
 ##can add a if tag in the template to hide salary and role from normal user
 @login_required
@@ -126,6 +133,7 @@ def update_profile(request):
         'form': profile_form
     })
 
+
 @transaction.atomic
 def register(request):
     context = {}
@@ -142,10 +150,10 @@ def register(request):
 
     ## create new_user
     new_user = User.objects.create_user(username=form.cleaned_data['user_name'], \
-                    first_name=form.cleaned_data['first_name'], \
-                    last_name=form.cleaned_data['last_name'], \
-                    password=form.cleaned_data['password1'],\
-                    email=form.cleaned_data['email'])
+                                        first_name=form.cleaned_data['first_name'], \
+                                        last_name=form.cleaned_data['last_name'], \
+                                        password=form.cleaned_data['password1'], \
+                                        email=form.cleaned_data['email'])
     new_user.is_active = False
     new_user.save()
 
@@ -168,6 +176,7 @@ http://%s%s
     context['email'] = form.cleaned_data['email']
     return render(request, 'SEI/needs-confirmation.html', context)
 
+
 @transaction.atomic
 def confirm_register(request, user_name, token):
     try:
@@ -187,10 +196,57 @@ def confirm_register(request, user_name, token):
             return redirect(reverse('home'))
         else:
             print("sorry")
-    except Exception as e: 
+    except Exception as e:
         print(e)
         return redirect(reverse('home'))
 
 
+@login_required
+def project_overview(request, PWP_num):
+    context = {}
+    project_item = get_object_or_404(Project, PWP_num=PWP_num)
+    charge_string = get_object_or_404(ChargeString, project=project_item)
+    context['PWP_num'] = project_item.PWP_num
+    context['project_description'] = project_item.project_description
+    context['project_budget'] = project_item.project_budget
+    context['isExternal'] = project_item.isExternal
+    context['team_name'] = project_item.team.team_name
+    context['organization_name'] = project_item.client.organization_name
+    context['start_date'] = project_item.start_date
+    context['end_date'] = project_item.end_date
+    context['charge_string'] = charge_string
+    return HttpResponse(json.dumps(context), content_type="application/json")
 
 
+@login_required
+def budget_view(request, PWP_num):
+    now = datetime.datetime.now()
+    context = {}
+    project_item = get_object_or_404(Project, PWP_num=PWP_num)
+    project_month = get_object_or_404(ProjectMonth, project=project_item)
+    context['total_budget'] = project_item.project_budget
+    total_expense = 0
+    total_expense_till_now = 0
+    monthly_expense = {}
+
+    for single_project_month in project_month:
+        budget_detail = {}
+        budget_detail['year'] = single_project_month.year
+        budget_detail['month'] = single_project_month.month
+        budget_detail['budget'] = single_project_month.budget
+        project_month_expense = get_object_or_404(ProjectExpense, project_month=single_project_month)
+        month_total_expense = 0
+        for month_expense_detail in project_month_expense:
+            month_total_expense += month_expense_detail.cost
+            total_expense += month_expense_detail.cost
+            if single_project_month.year < now.year or \
+                    (single_project_month.year == now.year and single_project_month.month <= now.month):
+                total_expense_till_now += month_expense_detail.cost
+        budget_detail['expense'] = month_total_expense
+        monthly_expense[str(single_project_month.year) + '.' + str(single_project_month.month)] = budget_detail
+
+    context['monthly_expense'] = monthly_expense
+    context['budget_balance'] = context['total_budget'] - total_expense_till_now
+    context['projected_expense'] = total_expense - total_expense_till_now
+    context['projected_remain'] = context['total_budget'] - total_expense
+    return HttpResponse(json.dumps(context), content_type="application/json")
